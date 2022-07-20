71c7ea5b139c0c608a2a174fe5c7c8931e0df80c
Ignatius Ealias Roy <ignatiusealiasroy@gmail.com>
2022-07-20 22:22:11 +0530

    Functions changed

--- Sum.java
+++ Sum.java
@@ -5,8 +5,25 @@ public class Sum{
         System.out.println("Enter two number");
         int a=sc.nextInt();
         int b=sc.nextInt();
-        int c=a+b;
-        System.out.println("Sum :"+c);
+        System.out.println("1.Sum \n2.Difference\n3.Multiply\n4.Division");
+        System.out.println("Enter your choice....");
+        int ch =sc.nextInt();
+        switch(ch){
+            case 1: 
+                   System.out.println(a+b);
+                    break;
+            case 2:
+            System.out.println(a-b);
+            break; 
+            case 3: 
+            System.out.println(a*b);
+                    break;
+            case 4:
+            System.out.println(a/b);
+            break;
+            default:
+            System.out.println("Invalid choice");
+        }
 
         
     }
